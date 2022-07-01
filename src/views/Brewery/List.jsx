7618b797d6ba239d58dc9309a6007a5014351643
Dillon <dillonrhamilton@gmail.com>
2022-07-01 12:11:02 -0400

    initial commit, add basic search

---
+++ src/views/Brewery/List.jsx
@@ -0,0 +1,45 @@
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+const BrewItem = ({ name, city, state, id }) => {
+  const link = `/breweries/${id}`
+  return (
+    <li>
+      <Link to={link}>{name}</Link> - {city}, {state}
+    </li>
+  )
+}
+
+const SEARCH_ENDPOINT = (search, perPage = 10) => `https://api.openbrewerydb.org/breweries/search?query=${search}&per_page=${perPage}`
+
+export default function BreweryList() {
+  const [brewlist, setBrewList] = useState([]);
+  const [search, setSearch] = useState('')
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch(SEARCH_ENDPOINT(search))
+      .then(res => res.json())
+      .then(res => setBrewList(res))
+      .catch(e => console.error('searching failed', e))
+  }
+
+  return (
+    <main>
+      <h1>Brewery Catalog</h1>
+      <form onSubmit={handleSubmit}>
+        <input type='text' name='search' placeholder='Find a brewery' value={search} onChange={handleSearch}/>
+        <button type='submit'>Search</button>
+        <button type='reset'>Reset</button>
+      </form>
+      <ul>
+        {brewlist.map(({ id, name, city, state}) => <BrewItem key={id} name={name} city={city} state={state} id={id} />)}
+      </ul>
+    </main>
+  );
+}
