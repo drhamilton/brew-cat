4286b8cb4fb74bdfad3a5fd2f3aa74dc4d8c4c58
Dillon <dillonrhamilton@gmail.com>
2022-07-01 12:17:51 -0400

    add detail fetch

--- src/views/Brewery/Detail.jsx
+++ src/views/Brewery/Detail.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+
+const DETAIL_ENDPOINT = (id) => `https://api.openbrewerydb.org/breweries/${id}`;
 
 export default function BreweryDetail() {
   const { id } = useParams();
+  const [detail, setDetail] = useState()
+
+  useEffect(() => {
+    fetch(DETAIL_ENDPOINT(id))
+      .then(res => res.json())
+      .then(res => setDetail(res))
+      .catch(e => console.error('fetching failed', e));
+  }, [id])
+
+  console.log(detail)
+
+  if (!detail) {
+    return null;
+  } 
 
   return (
     <main>
